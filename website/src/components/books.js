742aa689c9cc83455708b4273ecf10e2fe08bd0d
Chelsea Houser <chelsearhouser@gmail.com>
2021-10-16 19:36:53 -0500

    Adding local bookstore links
    This is actually really cool: https://bookstorelink.com/

--- website/src/components/books.js
+++ website/src/components/books.js
@@ -43,6 +43,27 @@ class Books extends Component {
   render() {
     return (
       <section id="books">
+        <div className="row localStore">
+          <div className="three columns header-col">
+            <h1>
+              <span>Shop Local</span>
+            </h1>
+          </div>
+
+          <div className="nine columns main-col">
+            <div className="row item">
+              <div className="twelve columns">
+              <div key="localStore">
+                <h3 onClick={()=> window.open("https://bookstorelink.com/", "_blank")}>Bookstore Link</h3>
+                <p className="info">Now more than ever our local independent bookstores can 
+                use our help. <a href="https://bookstorelink.com/">Bookstore Link</a> connects 
+                people to local stores to bring money back into their community. These are not affiliate links,
+                shop local and support your community bookstores.</p>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="row currentlyReading">
           <div className="three columns header-col">
             <h1>
